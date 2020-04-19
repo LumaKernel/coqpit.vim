d770c02fea2b5fdf89de128512c4c77c1fce3387
Luma <markermails@gmail.com>
2020-04-19 21:44:53 +0900

    debug

--- dev/README.md
+++ dev/README.md
@@ -4,5 +4,7 @@
 - [Coq XML Protocol Document](https://github.com/coq/coq/blob/master/dev/doc/xml-protocol.md)
 
 
-update to GHA
+# Feel free to update below to trigger GHA 
+
+ya; sleep; nul
 
--- test/themis-run.sh
+++ test/themis-run.sh
@@ -7,10 +7,10 @@ PATH=~/.local/bin/coq/V8.11.1/bin:$PATH
 themis --version
 
 export THEMIS_VIM="vim"
-export THEMIS_LOG_FILE="themis-vim.log"
+export THEMIS_LOG_FILE="coqpit-vim.log"
 themis "$@"
 
 export THEMIS_VIM="nvim"
 export THEMIS_ARGS="-e -s --headless"
-export THEMIS_LOG_FILE="themis-nvim.log"
+export THEMIS_LOG_FILE="coqpit-nvim.log"
 themis "$@"
